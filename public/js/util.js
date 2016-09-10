3ef47052c2943f7f0519cc9dac05afd3f3176fdd
Garrick <garrickthurston@gmail.com>
2016-09-10 17:20:44 -0500

    clean up

---
+++ public/js/util.js
@@ -0,0 +1,20 @@
+var closest = function(el, fn) {
+    return el && (
+        fn(el) ? el : closest(el.parentNode, fn)
+    );
+};
+
+var grep = function(items, callback) {
+    var filtered = [],
+        len = items.length,
+        i = 0;
+    for (i; i < len; i++) {
+        var item = items[i];
+        var cond = callback(item);
+        if (cond) {
+            filtered.push(item);
+        }
+    }
+
+    return filtered;
+};

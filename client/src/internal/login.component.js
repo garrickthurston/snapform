606814751c7005d7722fcedc116ea63b1a6de010
Garrick <garrickthurston@gmail.com>
2019-02-10 16:26:34 -0600

    login layout

--- client/src/internal/login.component.js
+++ client/src/internal/login.component.js
@@ -20,7 +20,9 @@ class LoginComponent extends Component {
 
         this.state = {
             email: '' ,
-            emailInputClassName: 'g-border-color'
+            password: '' ,
+            emailInputClassName: 'g-border-color',
+            passwordInputClassName: 'g-border-color'
         };
 
         const backgrounds = [bg_1, bg_2];
@@ -29,45 +31,66 @@ class LoginComponent extends Component {
         this.props.updateBGImage(`url('${url}')`);
 
         this.login = this.login.bind(this);
-        this.emailChange = this.emailChange.bind(this);
         this.handleInputClassName = this.handleInputClassName.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
     }
 
-    login() {
-
-    }
-
-    emailChange(e) {
-        this.setState({email: e.target.value});
-
-        this.handleInputClassName('email');
+    login(e) {
+        e.preventDefault();
     }
 
     handleInputClassName(field) {
         var name = 'g-border-color';
         switch (field) {
             case 'email':
-                name += !!this.state.email ? ' g-valid' : '';
-                break;
+                name += !!this.state.email && this.state.email.length ? ' g-valid' : '';
+                this.setState({emailInputClassName: name});
+                return;
+            case 'password':
+                name += !!this.state.password && this.state.password.length ? ' g-valid': '';
+                this.setState({passwordInputClassName: name});
+                return;
         }
-
-        this.setState({emailInputClassName: name});
     }
 
+    handleEmailChange(e) {
+        this.setState({email: e.target.value});
+
+        this.handleInputClassName('email');
+    }
+
+    handlePasswordChange(e) {
+        this.setState({password: e.target.value});
+
+        this.handleInputClassName('password');
+    }
     render() {
         const { backgroundImage } = store.getState();
-        const className = this.state.emailInputClassName;
+        const { emailInputClassName, passwordInputClassName } = this.state;
         return (
             <div className="g-container">
                 <div id="backgroundElement" className="bg" style={{ background: backgroundImage }}></div>
                 <div id="backgroundElementShade" className="shade"></div>
                 <div className="g-landing">
                     <div className="g-landing-logo"></div>
-                    <form className="g-login-form" onSubmit={this.login()}>
+                    <form className="g-login-form" onSubmit={this.login}>
                         <div className="g-login-form-input">
-                            <input className={className} type="text" value={this.state.email} onChange={this.emailChange} />
+                            <input className={emailInputClassName} type="text" value={this.state.email} onChange={this.handleEmailChange} />
                             <span className="g-login-form-input-placeholder">Email</span>
                         </div>
+                        <div className="g-login-form-input">
+                            <input className={passwordInputClassName} type="password" value={this.state.password} onChange={this.handlePasswordChange} />
+                            <span className="g-login-form-input-placeholder">Password</span>
+                        </div>
+                        <div className="g-btn-holder">
+                            <input className="g-btn" type="submit" value="Submit" />
+                        </div>
+                        {/* <input type="submit" [ngClass]="{'o-btn--dark': processing}" class="t-btn o-btn o-btn--maxinternal o-btn__dashboard--active o-btn--setheight" value="Login" [disabled]="!loginForm.valid || processing">  */}
+                        {/* <div class="c-login__forgot_container">
+                            <a class="c-login__forgot" routerLink="/forgotpassword">Forgot Password?</a>
+                            <h5 style="color: red;">{{messages}}</h5>
+                        </div> */}
                     </form>
                 </div>
             </div>

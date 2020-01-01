cf08a7b46762faba7a1d4311aff5902ba0dabe39
Garrick Thurston <garrickthurston@gmail.com>
2019-12-31 22:00:52 -0600

    starts ws nav

--- client/src/components/workspace/WorkspaceNavComponent.jsx
+++ client/src/components/workspace/WorkspaceNavComponent.jsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useWorkspace } from '../../contexts/providers/WorkspaceContextProvider';
 import './WorkspaceNavComponent.scss';
 
 export default function WorkspaceNavComponent() {
+    const workspace = useWorkspace();
+
+    const renderWorkspaces = useMemo(() => {
+        const { workspaces = [] } = workspace;
+
+        return workspaces.map((item) => {
+            const { projects } = item;
+
+            const renderProjects = () => {
+                if (projects.length) {
+                    return (
+                        <ul>
+                            {projects.map((project) => (
+                                <li key={project.projectId}>{project.projectName}</li>
+                            ))}
+                        </ul>
+                    );
+                }
+
+                return null;
+            };
+
+            return (
+                <li key={item.workspaceId}>
+                    {item.workspaceName}
+                    {renderProjects()}
+                </li>
+            );
+        });
+    }, [workspace]);
+
     return (
-        <div>PROJECT NAV GOES HERE</div>
+        <ul className="ws-root-nav">
+            {renderWorkspaces}
+        </ul>
     );
 }
